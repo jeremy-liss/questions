761d0bf848aa4688d8556ea6e960b0550f46d025
jeremy-liss <jeremyliss@gmail.com>
2017-03-03 10:44:10 +1300

    form html complete and route working hello world

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 var fs = require('fs')
 
 module.exports = {
-  home: home
+  home: home,
+  form: form,
 }
 
 function home (req, res) {
   res.send("Hello world?")
+}
+
+function form (req, res) {
+  res.send("hello, world")
 }
