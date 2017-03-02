988bc6869812584a2de597a32ad7dd30441078ef
student <poukupenga@gmail.com>
2017-03-03 10:51:33 +1300

    home page is rendering

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
+// var views = require('./views')
 
 module.exports = {
   home: home
 }
 
 function home (req, res) {
-  res.send("Hello world?")
+  res.sendFile(__dirname + '/views/home.html')
 }
